356970b53cb3f07095f267358dfdcac1198f22af
Tamara <yaroslavcevatamara@gmail.com>
2020-09-29 01:33:34 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let minValue = 0;
+  if (array === undefined) return 0;
+  if (array.length > 0) {
+    for (i = 0; i < array.length; i++) {
+      if (minValue > array[i]) {
+        minValue = array[i];
+      }
+    }
+    return minValue
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let maxValue = 0;
+  if (array === undefined) return 0;
+  if (array.length > 0) {
+    for (i = 0; i < array.length; i++) {
+      if (maxValue < array[i]) {
+        maxValue = array[i];
+      }
+    }
+    return maxValue
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined) return 0;
+  if (array.length > 0) {
+    return array.reduce((a, b) => a + b) / array.length;
+  } else {
+    return 0;
+  }
 }
